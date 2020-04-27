91335aee29d76b9f585d498584d2cadc55ca8975
margarettchen <mdc5bv@virginia.edu>
2020-04-27 14:47:20 -0400

    Created Angular component for food diary entry form

---
+++ order.ts
@@ -0,0 +1,10 @@
+export class Order {
+    constructor(
+       public food: string,
+       public calories: number,
+       public protein: number,
+       public carbohydrates: number,
+       public fat: number
+    ){}
+ }
+ 
