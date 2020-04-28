48d369bd586029740da00d8daa734c8b66c8396e
Jon Wen <41758091+JonSWen@users.noreply.github.com>
2020-04-27 21:17:13 -0400

    Changed form to use reference server.php
    Changed from using PDO to mysqli to access DB

--- form.php
+++ form.php
@@ -1,7 +1,4 @@
-<?php
-include('./connectdb.php');
-?>
-
+<?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +9,22 @@ include('./connectdb.php');
   <title>PHP and database</title>    
 </head>
 <body>
-  
+  <!----------Navbar---------->
+  <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+    <img src="logo.png" class="navbar-brand" style="width:150px"/>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="dashboard.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="personalinfo.html">Profile</a>
+        <a class="nav-item nav-link" href="form.php">Upload</a>
+        <a class="nav-item nav-link" href="#">Contact Us</a>
+        </div>
+    </div>
+    </nav>
+<!----------Navbar END---------->
   <div class="container">
   <br/>
   
@@ -49,33 +61,3 @@ include('./connectdb.php');
 </body>
 </html>
 
-<?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //server-side input validation to check non-null inputs
-        if (!empty($_POST['fooddesc']) && !empty($_POST['calories']))
-        {
-            global $db;
-            $test1 = $_POST['fooddesc'];
-            $test2 = $_POST['calories'];
-            $test3 = $_POST['protein'];
-            $test4 = $_POST['carbs'];
-            $test5 = $_POST['fat'];
-        
-            $query = "INSERT INTO foodentry (food, calories, protein, carbs, fat) VALUES ('$test1', '$test2', '$test3', '$test4', '$test5');";
-            $statement = $db->prepare($query);
-            if(!$statement->execute()) {
-                echo 'something went wrong here calories: ' . $test2;
-                print_r($statement->errorInfo());
-                
-            }     
-            // if the statement is successfully executed, execute() returns true
-            // false otherwise
-            
-            $statement->closeCursor();
-            //addTask($_POST['taskdesc'], $_POST['duedate'], $_POST['priority']);
-            //header("Location: main.php?action=list_tasks");
-        }
-    }
-
-
-?>
